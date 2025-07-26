dd7c488321ea115bb9768f122d468e7d3af6efa7
Ayushkumar34666 <dhruvkumar2264@gmail.com>
2025-07-26 16:59:05 +0530

    Add files via upload

---
+++ sharpx.py
@@ -0,0 +1,17 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY SharpX72
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("8330449600:AAH1eym0TqKeqlvVqPzzb5-Iy6bdUzbdwQI")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("SharpX72")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("7582524575")
+
+
+
+
+
+  
